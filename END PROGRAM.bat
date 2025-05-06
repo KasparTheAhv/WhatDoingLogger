167773cbe17ea55b099b77ad9bb5769ea98c2499
randoudris <randoudris@gmail.com>
2025-05-06 17:35:27 +0300

    Update END PROGRAM.bat

--- END PROGRAM.bat
+++ END PROGRAM.bat
@@ -4,4 +4,4 @@ echo Attempting to kill all pythonw.exe processes...
 powershell -NoProfile -Command "Get-CimInstance Win32_Process | Where-Object { $_.Name -eq 'pythonw.exe' } | ForEach-Object { try { Stop-Process -Id $_.ProcessId -Force -ErrorAction Stop } catch { Write-Host \"Failed to stop PID $($_.ProcessId): $_\" } }"
 
 echo Done.
-pause
+exit
