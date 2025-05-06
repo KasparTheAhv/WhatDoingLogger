aff3a7368859dc263c2fb61f738bcf9497d7aa29
randoudris <randoudris@gmail.com>
2025-05-06 16:45:01 +0300

    Update RUN PROGRAM.bat

--- RUN PROGRAM.bat
+++ RUN PROGRAM.bat
@@ -1,3 +1,11 @@
 @echo off
 cd /d "%~dp0"
+
+:: Check if pythonw is already running the script
+tasklist /fi "imagename eq pythonw.exe" | findstr /i "track.py" >nul
+if %errorlevel% equ 0 (
+    echo Track.py is already running!
+    exit /b
+)
+
 start "" /b pythonw "program\track.py"
